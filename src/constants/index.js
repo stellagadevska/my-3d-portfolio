cdaff977cab91781d6ff7d9bdf768cc40c43bc29
Stella Gadevska <58764584+stellagadevska@users.noreply.github.com>
2024-11-12 23:58:08 +0200

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,248 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  amdocs,
+  spge,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React.js Developer",
+    icon: mobile,
+  },
+  {
+    title: "FrontEnd Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Front End Developer",
+    company_name: "Amdocs",
+    icon: amdocs,
+    iconBg: "#E6DEDD",
+    date: "June 2021 - present",
+    points: [
+      "Designing, developing, modifying, debugging and/or maintaining software code according to functional, non-functional and technical design specifications.",
+      "Investigating issues by reviewing/debugging code, providing fixes and workarounds, and review changes for operability to maintain existing software solutions.",
+      "Working within a team, collaborating and adding value through participation in peer code reviews, providing comments and suggestions, and working with cross functional teams to achieve goals.",
+      "Technologies: HTML, CSS, JS fundamentals + ES6, React.JS, React Router, Redux, JSX, Git, npm, etc.",
+    ],
+  },
+  {
+    title: "IT Teacher",
+    company_name: "SPGE 'John Atanasov'",
+    icon: spge,
+    iconBg: "#383E56",
+    date: "December 2021 - January 2021",
+    points: [
+      "Teach Informatics, Programming Basics",
+      "Share professional experience and motivate students to develop IT skills",
+    ],
+  },
+  {
+    title: "FrontEnd Software Engineer Intern",
+    company_name: "Amdocs",
+    icon: amdocs,
+    iconBg: "#E6DEDD",
+    date: "March 2021 - June 2021",
+    points: [
+      "The focus of the internship was on Front-end technologies. Improved practical HTML, CSS, Vanilla JS, React.JS knowledge.",
+      "Built a project while learning React.JS, React Router, Redux and TypeScript.",
+      "Learned about agile methodologies like Scrum and Kanban",
+      "Skills improved: Analytical skills, Team work, Problem-solving capability, Self-motivation, Attention to detail, Time management",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
